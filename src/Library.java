eceb54e4ee73f0fafe0426843da3c299ce95378e
boug8352 <144700750+boug8352@users.noreply.github.com>
2025-03-07 14:24:24 -0500

    im scared
    its like halway done i think i was trying my best

---
+++ src/Library.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Library
+{
+    private ArrayList<String> librarylist;
+
+    public void library()
+    {
+        librarylist = new ArrayList<>();
+    }
+
+    public void addItem(String item)
+    {
+        librarylist.add(item);
+    }
+
+    public void removeItem(String item)
+    {
+        for (int i = 0; i < librarylist.size(); i++)
+        {
+            if (librarylist.get(i).contains(item))
+            {
+                librarylist.remove(i);
+            }
+        }
+
+    }
+
+    public string find(String item)
+    {
+        for (int i = 0; i < librarylist.size(); i++)
+        {
+            if (librarylist.get(i).contains(item))
+            {
+                return toString(i);
+            }
+            else
+            {
+                return 
+            }
+        }
+    }
+
+    public String getNextItem(String item)
+    {
+        return librarylist.get(0);
+    }
+
+    public void skipNextItem(String item)
+    {
+        librarylist.remove(0);
+    }
+
+
+}
